98463e65140979ba367128d72d6ba166ad28dc65
Liz Mendes <liz_mendes2001@yahoo.co.uk>
2017-05-21 10:52:15 +0100

    Add variables and scope chapters

---
+++ 05_scope/01_packageScope/02_visibility/main/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/LizzieM2003/udemyTraining/05_scope/01_packageScope/02_visibility/vis"
+)
+
+func main() {
+	fmt.Println(vis.MyName)
+	vis.PrintVar()
+}
---
+++ 05_scope/01_packageScope/02_visibility/vis/name.go
@@ -0,0 +1,5 @@
+package vis
+
+// MyName is exported
+var MyName = "Lizzie" // this will be exported
+var myPrivateName = "Fabulous Remoaner"
